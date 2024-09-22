5e048bc65b8ceae661f045e77edb33808086a193
Pranay <112006221+pranaydeep1@users.noreply.github.com>
2024-09-22 17:50:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     formData.append('data', data);
 
     try {
-      const res = await axios.post('http://localhost:5000/bfhl', formData, {
+      const res = await axios.post('https://bajaj-finserv-yv25.onrender.com/bfhl', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
